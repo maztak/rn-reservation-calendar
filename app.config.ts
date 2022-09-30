fa2fe27b098ace8a89c553f8dd7a9456d4547c69
maztak <takuya108817@gmail.com>
2022-09-30 17:35:50 +0900

    add app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,8 @@
+import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+  name: 'rn-reservation-calendar',
+  slug: 'rn-reservation-calendar',
+};
+
+export default config;
