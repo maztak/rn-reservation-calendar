d08cf33fdf73f5c391a46c694f49023fc322fdc9
maztak <takuya108817@gmail.com>
2022-09-30 22:26:53 +0900

    add heading with common style file

--- src/screens/ReservationScreen.tsx
+++ src/screens/ReservationScreen.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from '../styles/common.style';
 
 function ReservationScreen() {
   return (
     <View style={styles.container}>
-      <Text>Reservation Screen</Text>
+      <Text style={styles.heading5}>店舗選択</Text>
+      <Text style={styles.heading5}>日付選択</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default ReservationScreen;
---
+++ src/styles/common.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: '20px',
+    height: '100%',
+    backgroundColor: '#fff',
+  },
+  heading5: {
+    fontFamily: 'Noto Sans JP',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 19,
+    lineHeight: 28,
+    color: '#212529',
+    marginBottom: 40,
+  }
+});
+
+export default styles;
