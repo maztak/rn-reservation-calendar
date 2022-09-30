2784115141c3096460b981afbd3bd3b6f59c4448
maztak <takuya108817@gmail.com>
2022-09-30 22:09:32 +0900

    add react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { registerRootComponent } from "expo";
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { GRAPHQL_URL } from "@env";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ReservationScreen from './screens/ReservationScreen';
+
+const Stack = createNativeStackNavigator();
 
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
@@ -13,10 +18,11 @@ const client = new ApolloClient({
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Reservation" component={ReservationScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>      
   );
 }
